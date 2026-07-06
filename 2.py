8411b161e548a3f0d1ccd8156a760d3203da6992
krutarthparmar <krutarthparmar72@gmail.com>
2026-07-06 16:48:47 +0530

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+# Create array
+arr = np.array([10, 20, 30, 40, 50])
+
+# Indexing
+print(arr[2])      # 30
+
+# Slicing
+print(arr[1:4])    # [20 30 40]
+
+# Mathematical operations
+print(arr + 5)     # Add 5
+print(arr * 2)     # Multiply by 2
+print(arr.sum())   # Sum of elements
